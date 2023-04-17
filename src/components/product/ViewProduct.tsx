14ed5157615a9d3f22fc28385da3aa63b234925d
Raul <103614244+raulp25@users.noreply.github.com>
2023-04-17 02:52:04 -0600

    Added: product type

--- src/components/product/ViewProduct.tsx
+++ src/components/product/ViewProduct.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useSelector } from '@/redux/store';
 import parse from 'html-react-parser';
+import { Trl } from '@/@types/company';
 
 export const ViewProduct = () => {
   const { product } = useSelector(state => state.company);
-  const [productData, setProductData] = useState<{name: string, description: string, pic: string}>();
+  const [productData, setProductData] = useState<{name: string, description: string, pic: string, type: Trl}>();
 
   useEffect(() => {
     if(product === null)
@@ -14,6 +15,7 @@ export const ViewProduct = () => {
       ['name']: product.name!,
       ['description']: product.description!,
       ['pic']: product.picture!,
+      ['type']: product.type!,
     })
   }, [product])
   
@@ -26,7 +28,12 @@ export const ViewProduct = () => {
         </div>
         <div className='mt-3 px-5 flex flex-col items-start w-full'>
             <div className='text-base font-bold text-left'>
-                 {productData?.name ? parse(productData?.name) : ''}
+                 <p>
+                  {productData?.name ? parse(productData?.name) : ''}
+                 </p>
+                 <span className="bg-gray-200 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-gray-200 ">
+                    {productData?.type ? productData.type.name : ''}
+                 </span>  
             </div>
             <div className='text-sm mt-2 text-left'>    
               {productData?.description ? parse(productData?.description) : ''}
